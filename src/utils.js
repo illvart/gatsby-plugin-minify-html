dbbbc665748f4945913ec16c349333d6e7779e8e
Muhammad Nur Fuad <illvart.com@gmail.com>
2020-02-18 21:43:56 +0700

    Improve console

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const isObject = val => val !== null && typeof val === 'object';
 const isBoolean = val => val === false || val === true;
 
@@ -11,10 +9,10 @@ const deepMerge = (target = {}, source = {}) => {
       }
       deepMerge(target[key], source[key]);
     } else {
-      Object.assign(target, { [key]: source[key] });
+      Object.assign(target, {[key]: source[key]});
     }
   });
   return target;
 };
 
-module.exports = { isObject, isBoolean, deepMerge };
+module.exports = {isObject, isBoolean, deepMerge};
